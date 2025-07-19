ad0c3cf41d485bf2fedd4ad5359bb27c061b0988
Ivan Sokolovic <isokolovic1@gmail.com>
2025-07-19 21:10:52 +0200

    Added logging function

--- C/src/logger.c
+++ C/src/logger.c
@@ -1,5 +1,7 @@
 #include "logger.h"
 #include <stdio.h>
+#include <stdarg.h>
+#include <time.h>
 
 static FILE* log_file = NULL;
 
@@ -21,6 +23,41 @@ void close_logger(void){
     }
 }
 
+/// @brief Handle messages logging
+/// @param level Info / Warning / Error
+/// @param format Custom message
 void log_message(LogLevel level, const char* format, ...){
-    
+    if (!log_file) return;
+
+    const char* level_str;
+
+    //Convert message level to human readable format
+    switch (level) {
+        case LOG_INFO: level_str = "INFO"; break; 
+        case LOG_WARNING: level_str = "WARNING"; break;
+        case LOG_ERROR: level_str = "ERROR"; break;
+        default: level_str = "UNKNOWN"; break;
+    }
+
+    //Convert time to human readable
+    time_t now = time(NULL); 
+    struct tm* tm = localtime(&now); 
+    char time_str[20];
+    strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", tm);
+
+    //Save log messages to the log file
+    fprintf(log_file, "%s - %s - ", time_str, level_str);
+    va_list args;
+    va_start(args, format);
+    vfprintf(log_file, format, args);
+    va_end(args); 
+    fprintf(log_file, "\n"); 
+    fflush(log_file); 
+
+    //Log messages to the console
+    printf("%s - %s - ", time_str, level_str);
+    va_start(args, format);
+    vprintf(format, args);
+    va_end(args);
+    printf("\n");
 }
